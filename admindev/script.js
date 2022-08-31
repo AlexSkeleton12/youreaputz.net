0491ea60364fbdc4388f49a161aea1aca0e70e28
Alexander Shad <50224102+AlexSkeleton12@users.noreply.github.com>
2022-08-31 22:41:30 +0200

    Update script.js

--- admindev/script.js
+++ admindev/script.js
@@ -1,5 +1,12 @@
+/*
+Xj9ZMHqGR9HqhgDblEE4 - Weston Sleeman
+*/
 function submit() {
-  // Get req with token | document.getElementById('token')
-  document.write("Simulating server request.")
-  setTimeout(() => {  window.location.replace('http://3.12.12.129:8000'); }, 1000);
+  if (document.getElementById('token').value == "Xj9ZMHqGR9HqhgDblEE4") {
+    document.write("Redirecting...")
+    setTimeout(() => {  window.location.replace('http://3.12.12.129:8000'); }, 1000);
+  } else {
+    document.write("Token invalid. Please refresh this page and try again.")
+  }
+
 }
