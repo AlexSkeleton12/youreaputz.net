a2e5d899e5fd599c887b45f78b285b72aa14c4c9
Alexander Shad <50224102+AlexSkeleton12@users.noreply.github.com>
2022-09-02 23:47:26 +0200

    Update script.js

--- admindev/script.js
+++ admindev/script.js
@@ -1,5 +1,5 @@
 function submit() {
-  let response = fetch('https://backend.youreaputz.net:3000/getURL', {
+  let response = fetch('http://3.140.129.44:3000/getURL', {
       headers: {
         'auth': document.getElementById('token').value.toString()
       }
