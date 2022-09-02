1dba4d43ff976147fb13960d6aca149a8cc1ef98
Alexander Shad <50224102+AlexSkeleton12@users.noreply.github.com>
2022-09-02 20:44:26 +0200

    Backend Integration | admindev segment complete

--- admindev/script.js
+++ admindev/script.js
@@ -1,12 +1,9 @@
-/*
-Xj9ZMHqGR9HqhgDblEE4 - Weston Sleeman
-*/
 function submit() {
-  if (document.getElementById('token').value == "Xj9ZMHqGR9HqhgDblEE4") {
-    document.write("Redirecting...")
-    setTimeout(() => {  window.location.replace('http://3.12.12.129:8000'); }, 1000);
-  } else {
-    document.write("Token invalid. Please refresh this page and try again.")
-  }
-
+  let response = fetch('http://3.140.129.44:3000/getURL', {
+      headers: {
+        'auth': document.getElementById('token').value.toString()
+      }
+    }
+  ).then(response => response.text()).then(response => window.location.replace(response)
+  );
 }
